55735243c42edcec610a2e07b1c1a8562d007baf
Arpan <arpanchakraborty500@gmail.com>
2024-02-23 17:23:00 +0530

    expariment

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r', encoding='utf-8') as f:
     long_description = f.read()     
    
 
-__version__ = "0.0.1"
+__version__ = "0.0.2"
 REPO_NAME = "DatabaseConnceter"
 PKG_NAME= "databaseautomation"
 AUTHOR_USER_NAME = "arpanchakraborty23"
